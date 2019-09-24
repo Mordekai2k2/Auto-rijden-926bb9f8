a4e06bd4910384eb76479395aa3d2062ee1803e4
Randy Brouwer <2105070@talnet.nl>
2019-09-24 10:35:51 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hewwo my name is Mumfwr!! You wanna know if yew can ride a caww? OwO???   ";
+echo "How old are you? ";
+$Age = readline();
+if ($Age > 16.5)
+    echo "You can ride a caww!!! UwU";
+if ($Age < 16.5)
+    echo "Hahah!!! you cant fool me. im the coolest!! yuw not old enough!!!";
